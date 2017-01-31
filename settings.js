c0c5608031664e15d13ef431e49fc242a3a09745
Christopher Walker <cnwalker@uchicago.edu>
2017-01-31 02:53:27 -0600

    Added settings and styles files to reduce clutter

---
+++ settings.js
@@ -0,0 +1,5 @@
+var API_ROOT = 'https://bridgesapi-skypath.rhcloud.com/'
+
+module.exports = {
+    API_ROOT: API_ROOT
+}
---
+++ styles/question_feed.js
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+const questionFeed = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+
+    title: {
+        textAlign: 'left',
+        fontWeight: 'bold'
+    },
+
+    description: {
+        textAlign: 'left',
+
+    },
+
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+module.exports = {
+    questionFeed: questionFeed
+};
