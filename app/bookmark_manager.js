350f596cb08098d54ce45e365c3f940d2108f5f7
Christopher Walker <cnwalker@uchicago.edu>
2017-03-18 15:42:17 -0500

    added bookmark manager and screen

---
+++ app/bookmark_manager.js
@@ -0,0 +1,70 @@
+import SInfo from 'react-native-sensitive-info';
+
+function containsQuestion(question_id, bookmarks) {
+    for (var i = 0; i < bookmarks.length; i++) {
+        if (question_id == bookmarks[i].id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function retrieveBookmarks(callback) {
+    return SInfo.getItem('bookmarks', {
+        sharedPreferencesName: 'shared_preferences'
+    }).then((bookmarks) => {
+        console.log('bookmarks', bookmarks);
+        if (bookmarks) {
+            console.log('tryna parse?');
+            callback(JSON.parse(bookmarks));
+            return;
+        }
+        callback([]);
+    });
+}
+
+function addBookmark(question) {
+    retrieveBookmarks(function(bookmarks) {
+         if (bookmarks) {
+             if (!containsQuestion(question.id, bookmarks)) {
+                 SInfo.setItem('bookmarks', JSON.stringify(bookmarks.concat(question)), {
+                     sharedPreferencesName: 'shared_preferences'
+                 });
+             }
+         } else {
+             console.log('new bookmarks list');
+             // Create a bookmarks list with a single question
+             SInfo.setItem('bookmarks', JSON.stringify([question]), {
+                  sharedPreferencesName: 'shared_preferences'
+             });
+         }
+     });
+}
+
+function removeBookmark(question_id) {
+    retrieveBookmarks(function(bookmarks) {
+        if (bookmarks) {
+            var filteredBookmarks = bookmarks.filter(function(bookmark){
+                return bookmark.id != question_id;
+            });
+
+            SInfo.setItem('bookmarks', JSON.stringify(filteredBookmarks), {
+                 sharedPreferencesName: 'shared_preferences'
+            });
+        }
+    });
+}
+
+function clearBoomarks() {
+    SInfo.deleteItem('bookmarks', {
+        sharedPreferencesName: 'shared_preferences'
+    });
+    console.log('cleared bookmarks');
+}
+
+module.exports = {
+    retrieveBookmarks: retrieveBookmarks,
+    addBookmark: addBookmark,
+    removeBookmark: removeBookmark,
+    clearBoomarks: clearBoomarks
+}
---
+++ app/screens/BookmarksScreen.js
@@ -0,0 +1,144 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  Image,
+  TouchableOpacity,
+  Button,
+  Alert,
+  SegmentedControlIOS
+} from 'react-native';
+import ViewContainer from '../components/ViewContainer'
+import StatusBarBackground from '../components/StatusBarBackground'
+
+var bridges_client = require('../bridges_client');
+var bookmark_manager = require('../bookmark_manager');
+
+const response = []
+
+export default class BookmarkScreen extends Component {
+constructor(props) {
+    super(props)
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2})
+    this.state = {
+      'response': [],
+      peopleDataSource: ds.cloneWithRows(response),
+      'searchTerm': null
+    }
+  }
+
+  _getQuestions() {
+      bookmark_manager.retrieveBookmarks(function(bookmarks) {
+          this.setState({
+              'response': bookmarks,
+              peopleDataSource: this.state.peopleDataSource.cloneWithRows(bookmarks)
+          });
+      }.bind(this));
+  }
+
+  _showTags(tags) {
+      return tags.map(function(tag) {
+          return tag.value
+      }).join(", ")
+  }
+
+  render() {
+    this._getQuestions();
+    return (
+     <ViewContainer>
+        <StatusBarBackground style={{backgroundColor: '#00857c'}}/>
+        <Text style={{height:40, textAlign: "center", backgroundColor: "#00857c",
+            fontSize: 22, color: "white", fontWeight: "bold"}}> Bookmarks </Text>
+        <ListView
+          dataSource = {this.state.peopleDataSource}
+          renderRow={(question) => {return this._renderPersonRow(question)}}
+          automaticallyAdjustContentInsets={false}
+          style = {{marginBottom: 50}}
+          renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}/>
+      </ViewContainer>
+    );
+  }
+
+  _renderPersonRow(question) {
+    return (
+      <TouchableOpacity style={styles.personRow} onPress={(event) => this._navigateToPersonShow(question) }>
+        <Text style={styles.questionTags}> Tags: {this._showTags(question.tags)}</Text>
+        <Text style={styles.questionTitle}> {question.title}</Text>
+        <View style={{flexDirection:"row", marginTop:5, marginBottom:5}}>
+          <Image source={require('./face.jpg')} style={styles.photo} />
+          <Text style={{fontSize:12, marginTop: 10}}> Rachel Mills </Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  _navigateToPersonShow(question) {
+    this.props.navigator.push({
+      ident: "PersonShow",
+      question: question,
+  })
+}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  personRow: {
+    flexDirection: "column",
+    alignSelf: 'stretch',
+    marginLeft: 7,
+    marginRight: 7
+  },
+  personName: {
+    marginTop: 15,
+    marginLeft: 10,
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  photo: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  separator: {
+    flex: 1,
+    height: 4,
+    backgroundColor: 'lightgray',
+  },
+  questionTitle: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+   questionDesc: {
+    fontSize: 15
+  },
+  questionTags: {
+    marginTop: 2,
+    marginBottom: 2,
+    fontSize: 12,
+    color: "grey"
+  }
+});
+
+module.exports = BookmarkScreen
