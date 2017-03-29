613beb261282ebe4b5c0926fa5efe6bc436a6767
Christopher Walker <cnwalker@uchicago.edu>
2017-03-29 16:09:42 -0500

    Now supporting question descriptions on app

--- app/screens/PersonShowScreen.js
+++ app/screens/PersonShowScreen.js
@@ -27,6 +27,7 @@ export default class PersonShowScreen extends Component {
         <StatusBarBackground style={{backgroundColor: '#00857c'}}/>
         <Text style={{height:40, textAlign: "center", backgroundColor: "#00857c",fontSize: 22, color: "white", fontWeight: "bold"}}>Answer</Text>
         <Text style={{marginTop: 20, marginLeft: 15, fontSize: 35, fontWeight: 'bold'}}>{this.props.question.title}</Text>
+        <Text style={styles.description}>{this.props.question.description}</Text>
         <View style={{flexDirection:"row", marginTop:5, marginBottom:5}}>
           <Image source={require('./face.jpg')} style={styles.photo} />
           <Text style={{fontSize:15, marginTop: 30}}> Rachel Mills </Text>
@@ -41,6 +42,12 @@ const styles = StyleSheet.create({
   personName: {
     fontWeight: "bold"
   },
+
+  description: {
+      marginTop: 20,
+      marginLeft: 15
+  },
+
   photo: {
     height: 50,
     width: 50,
