2212fef7da57ede7a79464544c470a365abb114c
Christopher Walker <cnwalker@uchicago.edu>
2017-04-20 14:40:57 -0500

    renamed app to Employable

--- ios/EmployableTests/EmployableTests.m
+++ ios/EmployableTests/EmployableTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface BridgesAppExampleTests : XCTestCase
+@interface EmployableTests : XCTestCase
 
 @end
 
-@implementation BridgesAppExampleTests
+@implementation EmployableTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
